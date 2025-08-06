ff107d0eb6d8ee76390945eb630edd81f1baa3d7
nargis7 <ronaldonargis786@gmail.com>
2025-08-06 06:25:11 +0530

    Added selection sort

---
+++ src/SelectionSort.java
@@ -0,0 +1,30 @@
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            // Find index of minimum element in unsorted part
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap minimum with current position
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 4, 2};
+        selectionSort(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
