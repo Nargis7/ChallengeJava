1a91fd2c9f726a1ae221aea4366f97b54eeb7943
nargis7 <ronaldonargis786@gmail.com>
2025-08-13 05:35:40 +0530

    Array problem

---
+++ src/SmallestLargest.java
@@ -0,0 +1,17 @@
+public class SmallestLargest {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50,90};
+        int max = arr[0];
+        int min = arr[0];
+        System.out.print(arr[arr.length-1]);//90 gives last element
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            } else{
+                min = arr[i];
+            }
+        }
+        System.out.println("Maximum array: " + max); // output:90
+        System.out.println("Minimum array: " + min); // output : 10
+    }
+}
---
+++ src/Swaps.java
@@ -0,0 +1,17 @@
+public class Swaps {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int start = 0 ;
+        int end = arr.length-1;
+        while(start<end){
+            int temp = arr[start];
+            arr[start] =  arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        for(int num: arr){
+            System.out.print(num + " "); //output: 5,4,3,2,1
+        }
+    }
+}
