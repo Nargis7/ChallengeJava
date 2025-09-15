0a266ddf02de1148db1cbf036175c6fd4d0fd49a
nargis7 <ronaldonargis786@gmail.com>
2025-09-15 10:42:10 +0530

    java question

---
+++ src/Convert.java
@@ -0,0 +1,18 @@
+public class Convert {
+    public static void main(String[] args) {
+//        int num = 1234;
+//        String str = String.valueOf(num); convert integer to string
+//        String str = Integer.toString(num); // convert integer to string
+//        System.out.println(str);
+        String str = "Java programming";
+//        System.out.println(str.length());//16
+//        System.out.println(str.charAt(5)); // p
+//        System.out.println(str.substring(3,7)); // a pr
+//        System.out.println(str.toLowerCase());
+//        System.out.println(str.toUpperCase());
+//        System.out.println(str.equals());
+//        System.out.println(str.trim());
+//        System.out.println(str.contains("gram"));
+//        System.out.println(str.replace("Java","js"));
+    }
+}
