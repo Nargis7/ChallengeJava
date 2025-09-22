efe10c54ec840b578341f3b9d6ea18433ad82eba
nargis7 <ronaldonargis786@gmail.com>
2025-09-22 12:37:58 +0530

    maths in java

---
+++ src/CheckPrime.java
@@ -0,0 +1,15 @@
+public class CheckPrime {
+    public static void main(String[] args) {
+        int n = 42;
+        System.out.println(isPrime(n));
+    }
+    public static boolean isPrime(int n){
+        if(n<=1) return false;
+        for(int i=2; i*i<=n; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
